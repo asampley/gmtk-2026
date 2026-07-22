ca28786d3f8fc0cde6908888f085bd94f87e8e64
Alex Sampley <alex.sampley@gmail.com>
2026-07-22 16:13:17 -0600

    Reconnect signals

--- game/ui/main_menu/main_menu.gd
+++ game/ui/main_menu/main_menu.gd
@@ -12,6 +12,7 @@ func _on_settings_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_help_button_pressed() -> void:
+	print("here")
 	help_menu.visible = true
 
 func _on_play_button_pressed() -> void:
