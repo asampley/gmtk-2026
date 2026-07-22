3f6ee22b2c53d81cad6d7e36916aaf1023edcfac
Alex Sampley <alex.sampley@gmail.com>
2026-07-22 17:17:51 -0600

    Flesh out resources

--- game/gameplay/reagent.gd
+++ game/gameplay/reagent.gd
@@ -1,2 +1,5 @@
 class_name Reagent
 extends Resource
+
+@export var name: String
+@export var icon: Texture2D
--- game/gameplay/tool.gd
+++ game/gameplay/tool.gd
@@ -1,2 +1,5 @@
 class_name Tool
 extends Resource
+
+@export var name: String
+@export var icon: Texture2D
