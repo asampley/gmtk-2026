03113b62095161fae0ff26ee79dd593b34c874c7
Alex Sampley <alex.sampley@gmail.com>
2026-07-22 19:58:57 -0600

    Add x and flip buttons

--- game/ui/recipe_book.gd
+++ game/ui/recipe_book.gd
@@ -6,11 +6,17 @@ var recipes: Array[Recipe]
 var page_flip: int = 0
 @onready var page_left: RecipeDiagram = %RecipeLeft
 @onready var page_right: RecipeDiagram = %RecipeRight
+@onready var turn_page_forward: Button = %TurnPageForward
+@onready var turn_page_backward: Button = %TurnPageBackward
+
+static func _render_recipe(page: RecipeDiagram, recipe: Recipe) -> void:
+	page.render_recipe(recipe)
 
 func _ready() -> void:
 	for resource: Resource in ResourceDataHandler.resource_dict["recipes"]:
 		recipes.append(resource as Recipe)
 
+	set_page_flip(0)
 	render_recipes()
 
 func render_recipes() -> void:
@@ -24,5 +30,16 @@ func render_recipes() -> void:
 	else:
 		_render_recipe(page_right, null)
 
-static func _render_recipe(page: RecipeDiagram, recipe: Recipe) -> void:
-	page.render_recipe(recipe)
+func _on_close_pressed() -> void:
+	visible = false
+
+func _on_turn_page_forward_pressed() -> void:
+	set_page_flip(page_flip + 1)
+
+func _on_turn_page_backward_pressed() -> void:
+	set_page_flip(page_flip - 1)
+
+func set_page_flip(i: int) -> void:
+	page_flip = clamp(0, (recipes.size() + 1) / 2, i)
+	turn_page_forward.disabled = page_flip >= (recipes.size() - 1) / 2
+	turn_page_backward.disabled = page_flip == 0
