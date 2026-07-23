6fda73f273182d1774f85d9ba3bc57976565235c
Alex Sampley <alex.sampley@gmail.com>
2026-07-22 19:32:25 -0600

    Add reaction icon to differentiate layers

--- game/ui/recipe_book.gd
+++ game/ui/recipe_book.gd
@@ -16,9 +16,13 @@ func _ready() -> void:
 func render_recipes() -> void:
 	if recipes.size() > page_flip * 2:
 		_render_recipe(page_left, recipes[page_flip * 2])
+	else:
+		_render_recipe(page_left, null)
 
 	if recipes.size() > page_flip * 2 + 1:
 		_render_recipe(page_right, recipes[page_flip * 2 + 1])
+	else:
+		_render_recipe(page_right, null)
 
 static func _render_recipe(page: RecipeDiagram, recipe: Recipe) -> void:
 	page.render_recipe(recipe)
--- game/ui/recipe_diagram.gd
+++ game/ui/recipe_diagram.gd
@@ -3,28 +3,33 @@ extends Control
 
 @onready var title: RichTextLabel = %Title
 @onready var tool_icon: TextureRect = %ToolIcon
+@onready var reaction_icon: Control = %ReactionIcon
 @onready var recipe_reagents: Control = %RecipeReagents
 @onready var recipe_products: Control = %RecipeProducts
 
 func render_recipe(recipe: Recipe) -> void:
-	print("Rendering recipe: ", recipe)
-
-	title.text = recipe.name
-	tool_icon.texture = recipe.tool.icon
-
 	for child in recipe_reagents.get_children():
 		child.queue_free()
 	for child in recipe_products.get_children():
 		child.queue_free()
 
-	for reagent in recipe.reagents:
-		var texture_rect := TextureRect.new()
-		texture_rect.texture = reagent.icon
-		recipe_reagents.add_child(texture_rect)
-		print(reagent)
+	reaction_icon.visible = recipe != null
 
-	for product in recipe.products:
-		var texture_rect := TextureRect.new()
-		texture_rect.texture = product.icon
-		recipe_products.add_child(texture_rect)
-		print(product)
+	if recipe == null:
+		title.text = "";
+		tool_icon.texture = null;
+	else:
+		title.text = recipe.name
+		tool_icon.texture = recipe.tool.icon
+
+		for reagent in recipe.reagents:
+			var texture_rect := TextureRect.new()
+			texture_rect.texture = reagent.icon
+			recipe_reagents.add_child(texture_rect)
+			print(reagent)
+
+		for product in recipe.products:
+			var texture_rect := TextureRect.new()
+			texture_rect.texture = product.icon
+			recipe_products.add_child(texture_rect)
+			print(product)
