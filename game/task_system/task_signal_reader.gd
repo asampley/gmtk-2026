03f4475496029fd1d9b9e031f7f381560280a513
dadames <dadames999@gmail.com>
2026-07-24 11:16:34 -0600

    Fixed bug from changes

--- game/task_system/task_signal_reader.gd
+++ game/task_system/task_signal_reader.gd
@@ -8,7 +8,7 @@ extends Node
 func initialize(tool_in: Tool) -> void:
 	tool_in.updated_reagents.connect(on_updated_reagents)
 
-func on_updated_reagents(reagents: Array[Reagent]) -> void:
-	for reagent: Reagent in reagents:
+func on_updated_reagents(reagents_to_progress: Dictionary[Reagent, ReactionProgress]) -> void:
+	for reagent: Reagent in reagents_to_progress.keys():
 		if reagent_to_index.has(reagent):
 			EventBus.game_events.task_completed.emit(reagent_to_index[reagent])
