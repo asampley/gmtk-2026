fcbd2056cb0036f756ca3627bacba6c9245d49ee
Alex Sampley <alex.sampley@gmail.com>
2026-07-25 12:26:29 -0600

    Set default volume with volume sliders rather than reading

--- game/ui/hud/music_control.gd
+++ game/ui/hud/music_control.gd
@@ -3,7 +3,7 @@ extends HSlider
 var music_bus := AudioServer.get_bus_index("Music")
 
 func _on_ready() -> void:
-	value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	_on_value_changed(value)
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
--- game/ui/hud/sfx_control.gd
+++ game/ui/hud/sfx_control.gd
@@ -3,7 +3,7 @@ extends HSlider
 var sfx_bus := AudioServer.get_bus_index("Game_Audio")
 
 func _on_ready() -> void:
-	value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
+	_on_value_changed(value)
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
