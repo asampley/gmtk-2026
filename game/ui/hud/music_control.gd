f92bd4dc0d485bed8356ae33f6d7624e27fadcff
zedoom <harrydalbel@gmail.com>
2026-07-24 18:57:39 -0600

    Update music_control.gd

--- game/ui/hud/music_control.gd
+++ game/ui/hud/music_control.gd
@@ -1,9 +1,9 @@
 extends HSlider
 
-var sfx_bus := AudioServer.get_bus_index("Music")
+var music_bus := AudioServer.get_bus_index("Music")
 
 func _on_ready() -> void:
-	value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
+	value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
 
 func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
